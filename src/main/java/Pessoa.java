bd7d48d7fd5020a5a1686c3045fa258f52646ca4
Fabriciooml <Sabj1401>
2022-07-20 21:50:22 -0300

    remove old project

--- src/main/java/Pessoa.java
+++
@@ -1,5 +0,0 @@
-public class Pessoa {
-    public String reclamar(Reclamado reclamado, String mensagem) {
-        return CentralReclamacoes.getInstancia().receberReclamacao(reclamado, mensagem);
-    }
-}
--- src/main/java/Reclamado.java
+++
@@ -1,3 +0,0 @@
-public interface Reclamado {
-    String receberReclamacao(String mensagem);
-}
